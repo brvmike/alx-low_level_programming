fcdf724b34ec0dd0732651293102bfe05ac21be8
brvmike <bravemike7@live.com>
2022-12-11 08:32:00 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
  *
@@ -9,6 +10,7 @@
 int main(void)
 {
 	char low;
+
 	for (low = 'a'; low <= 'z'; low++)
 	{
 		if (low != 'q' && low != 'e')
