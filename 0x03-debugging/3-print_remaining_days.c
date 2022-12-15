0b07d9b71a4aa5213e19d80eff5b1b81c62e38bc
brvmike <bravemike7@live.com>
2022-12-15 15:29:53 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -30,8 +30,7 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
+			printf("Day of the year: %d\n", da      "Remaining days: %d\n", 365 - day);
 		}
 	}
 }
