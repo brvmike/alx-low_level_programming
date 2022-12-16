1451cb17b85a1ce411bdd640a9a27fc06d82aec1
brvmike <bravemike7@live.com>
2022-12-16 13:40:08 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ * Description: checks if a character is uppercase
+ * @c: the integer value is receives
+ * Return: 1 if true. 0 if fales
+ */
+
+int _isupper(int c)
+{
+	int i = 'A';
+
+	for (i = 'A'; i <= 'Z'; i++;)
+	{
+		/* refer int c*/
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
